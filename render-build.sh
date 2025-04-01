121b7c96eff26b828fc2627c565a8ffbe3f7c1bc
pp-reema <reemapp88@gmail.com>
2025-04-01 19:16:05 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
-# Install the correct Python version
-pyenv install 3.12.9
-pyenv global 3.12.9
+#!/bin/bash
 
-# Install system dependencies
-apt-get update && apt-get install -y portaudio19-dev
+# Install PortAudio dependencies
+apt-get update
+apt-get install -y portaudio19-dev python3-pyaudio
 
 # Install Python dependencies
 pip install -r requirements.txt
